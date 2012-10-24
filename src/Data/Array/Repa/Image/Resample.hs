95c12b4038a75366ab76507be08498c162f13e3c
Jonathan Fischoff <jonathangfischoff@gmail.com>
2012-10-24 09:17:29 -0700

    check point before branch

--- src/Data/Array/Repa/Image/Resample.hs
+++ src/Data/Array/Repa/Image/Resample.hs
@@ -3,6 +3,9 @@ module Data.Array.Repa.Image.Resample where
 import Data.Array.Repa
 import Data.Array.Repa.Unsafe
 import Data.Word
+--import Data.Array.Repa.Specialised.Dim2
+
+--TODO first change the image to use 
 
 data Size  = Size !Int !Int
 
@@ -37,13 +40,21 @@ toDouble f (DIM3Neighborhood {..}) = DoubleNeighborhood
 resize :: Source r Word8 => Array r DIM3 Word8 -> Size -> Array D DIM3 Word8
 resize image newSize@(Size w h) = traverse image resizeShape elemFn where
     elemFn get (Z :. hi :. wi :. p) = bilinear newIndex neighborhood where 
-        newIndex@(Index nwi nhi) = remapIndex inputSize newSize $
-                                    Index (fromIntegral wi) (fromIntegral hi)
-        neighborhood             = toDouble get . closestFour $
+        newIndex@(Index nwi nhi) = remapIndex oldW oldH w h (fromIntegral wi) (fromIntegral hi)
+        neighborhood             = toDouble get $ closestFour
                                          (Z :. ceiling nhi :. ceiling nwi :. p)
     {-# INLINE resizeShape #-}
     resizeShape (Z :. _ :. _ :. x) = Z :. h :. w :. x
-    inputSize                      = toSize . listOfShape . extent $ image
+    [_, oldW, oldH]                = listOfShape $ extent image
+
+{-# INLINE remapIndex #-}    
+remapIndex :: Int -> Int -> Int -> Int -> Float -> Float -> Index 
+remapIndex oldW oldH w h x y = 
+    Index ((oldW </> w) * x) ((oldH </> h) * y) where 
+
+        {-# INLINE (</>) #-}
+        a </> b = fromIntegral a / fromIntegral b
+    
 {-# INLINE resize #-}
 
 bilinear :: Index -> DoubleNeighborhood -> Word8 
@@ -51,21 +62,14 @@ bilinear (Index {..}) (DoubleNeighborhood {..}) = ceiling $
     dBottomRight * (1 - x') * (1 - y') + dBottomLeft * x' * (1 - y') +
     dTopRight    * (1 - x') * y'       + dTopLeft    * x' * y' where
     
-    y' = y - (fromIntegral . floor $ y)
-    x' = x - (fromIntegral . floor $ x)
+    y' = y - (fromIntegral $ floor y)
+    x' = x - (fromIntegral $ floor x)
 {-# INLINE bilinear #-}
 
 toSize :: [Int] -> Size
 toSize [_, w, h] = Size w h
 {-# INLINE toSize #-}
 
-remapIndex :: Size -> Size -> Index -> Index 
-remapIndex (Size oldWidth oldHeight) (Size newWidth newHeight) (Index {..}) = 
-    Index ((oldWidth </> newWidth) * x) ((oldHeight </> newHeight) * y) where 
-
-        {-# INLINE (</>) #-}
-        a </> b = fromIntegral a / fromIntegral b
-{-# INLINE remapIndex #-}
     
 closestFour :: DIM3 -> DIM3Neighborhood 
 closestFour (Z :. i :. j :. p) = 
